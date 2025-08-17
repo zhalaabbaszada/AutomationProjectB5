052a16c7dc2272a5322c6812e92065d569b6f8c7
zhalaabbaszada <zhalaabbaszada@gmail.com>
2025-08-17 10:02:43 -0400

    day 5

--- src/test/java/io/loop/test/utilisies/LoopCampConstants.java
+++ src/test/java/io/loop/test/utilisies/LoopCampConstants.java
@@ -4,7 +4,6 @@ public class LoopCampConstants {
     // url for registration form
     public static final String REGISTRATION_FORM_URL = "https://loopcamp.vercel.app/registration_form.html";
     public static final String RADIOBUTTON_URL = " https://loopcamp.vercel.app/radio-buttons.html";
-
     //messages
     public static final String EXCEPTED_HEADER_fOR_fORM = "Registration form";
     public static final String EXPECTED_PLACEHOLDER_FOR_FIRSTNAME = "first name";
